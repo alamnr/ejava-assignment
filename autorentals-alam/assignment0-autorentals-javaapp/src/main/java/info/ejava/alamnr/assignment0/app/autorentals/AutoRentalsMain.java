be43bbad7a875f86122f4514994f5fafa65350f1
alamnr <alamnr@gmail.com>
2025-03-19 21:43:53 +0600

    build spring boot executable jar

--- autorentals-alam/assignment0-autorentals-javaapp/src/main/java/info/ejava/alamnr/assignment0/app/autorentals/AutoRentalsMain.java
+++ autorentals-alam/assignment0-autorentals-javaapp/src/main/java/info/ejava/alamnr/assignment0/app/autorentals/AutoRentalsMain.java
@@ -2,7 +2,7 @@ package info.ejava.alamnr.assignment0.app.autorentals;
 
 public class AutoRentalsMain {
 
-    public static void main (String ...args){
+    public static void main (String...   args){
         System.out.println("AutoRentals has started");
     }
 
