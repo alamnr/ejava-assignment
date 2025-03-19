842aea1b34dc1f098d2888e89be685335ee23589
alamnr <alamnr@gmail.com>
2025-03-19 17:37:45 +0600

    initial commit

---
+++ autorentals-alam/assignment0-autorentals-javaapp/src/main/java/info/ejava/alamnr/assignment0/app/autorentals/AutoRentalsMain.java
@@ -0,0 +1,9 @@
+package info.ejava.alamnr.assignment0.app.autorentals;
+
+public class AutoRentalsMain {
+
+    public static void main (String ...args){
+        System.out.println("AutoRentals has started");
+    }
+
+}
