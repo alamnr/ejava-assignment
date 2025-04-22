89af825ab60f8f5ebe96c66210748b0edf23ca66
alamnr <alamnr@gmail.com>
2025-04-22 09:19:58 +0600

    added Mock testing for Renter Validator and updated to use @InjectMock , and Mockito then() , should() instead of verify

--- autorentals-alam/assignment1-autorentals-testing/src/test/java/info/ejava/alamnr/assignment1/testing/autorentals/RentersServiceMockedTest.java
+++ autorentals-alam/assignment1-autorentals-testing/src/test/java/info/ejava/alamnr/assignment1/testing/autorentals/RentersServiceMockedTest.java
@@ -23,6 +23,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import info.ejava.assignments.testing.rentals.renters.InvalidInputException;
 import info.ejava.assignments.testing.rentals.renters.RenterDTO;
 import info.ejava.assignments.testing.rentals.renters.RenterValidator;
 import info.ejava.assignments.testing.rentals.renters.RentersProperties;
@@ -101,7 +102,7 @@ public class RentersServiceMockedTest {
                                 .as("errMsg list is not empty hence size is not Zero ")
                                 .isEqualTo(0);
         BDDAssertions.assertThat(validRenterReturned.getId())
-                        .as("added/returned renter id is not null/empty/blank")
+                        .as("added/returned renter id is  null/empty/blank")
                         .isNotNull();
     }
 
@@ -109,13 +110,49 @@ public class RentersServiceMockedTest {
     void rejects_invalid_renter(){
 
         // given / arrange
-
-        
+        validRenter = RenterDTO.builder().firstName("").lastName("buffet").dob(LocalDate.now()).build();
+        // define behavior of mock during test 
+        BDDMockito.when(validatorMock.validateNewRenter(renterDtoCaptor.capture(), intCaptor.capture()))
+                        .thenReturn( List.<String>of("renter.firstName is null"));
+        // BDDMockito.doReturn(Collections.<String> emptyList())
+        //              .when(validatorMock.validateNewRenter(renterDtoCaptor.capture(), intCaptor.capture()));
 
         // when / act
-
+        // conduct test
+        Throwable ex  = BDDAssertions.catchThrowableOfType(InvalidInputException.class, () -> rentersService.createRenter(validRenter));
+        List<String> errMsgs = validatorMock.validateNewRenter(validRenter, renterProps.getMinAge());
+        
 
         // then / assert
+        // evaluate result
+        verify(validatorMock,times(2)).validateNewRenter(any(RenterDTO.class), anyInt()); // verify called once
+        BDDMockito.then(validatorMock).should(times(2)).validateNewRenter(validRenter, renterProps.getMinAge());
+
+        // verify what was given to mock
+        log.info("renter props' age - {}", renterProps.getMinAge());
+        log.info("added RenterDTO - {}", ex);
+        log.info("renter dto captors size - {}" , renterDtoCaptor.getAllValues().size());
+        //BDDAssertions.assertThat(renterDtoCaptor.getValue().getId())
+        BDDAssertions.assertThat(renterDtoCaptor.getAllValues().get(0).getId())
+        //BDDAssertions.assertThat(renterDtoCaptor.getAllValues().get(1).getId())
+                        .as("renter id is not null/empty/blank")
+                        .isNull(); // .isBlank()
+        
+        log.info("int captors size- {}" , intCaptor.getAllValues().size());
+        //BDDAssertions.assertThat(intCaptor.getValue()).as("minAge is not equal %d", renterProps.getMinAge())
+        BDDAssertions.assertThat(intCaptor.getAllValues().get(0)).as("minAge is not equal %d", renterProps.getMinAge())
+        //BDDAssertions.assertThat(intCaptor.getAllValues().get(1)).as("minAge is not equal %d", renterProps.getMinAge())
+                        .isEqualTo(renterProps.getMinAge());
+        // verify what was returned by mock
+
+        BDDAssertions.assertThat(errMsgs.size())
+                                .as("errMsg list is not empty hence size is not Zero ")
+                                .isEqualTo(1);
+        BDDAssertions.assertThat(ex.getMessage())
+                        .as("doesnot contain - renter.firstName")
+                        .contains("renter.firstName");
+
+
     }
     
 }
