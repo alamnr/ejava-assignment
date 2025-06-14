1fcbb4c333ea98728a64b3d44557becaabb82b3c
alamnr <alamnr@gmail.com>
2025-06-14 20:37:32 +0600

    RenterController Web MVC Mock Unit Test

--- autorentals-alam/assignment2-autorentals-api/autorentals-api-main/src/test/java/info/ejava/assignments/api/autorentals/svc/main/renter/client/mock/RenterControllerMockedNTest.java
+++ autorentals-alam/assignment2-autorentals-api/autorentals-api-main/src/test/java/info/ejava/assignments/api/autorentals/svc/main/renter/client/mock/RenterControllerMockedNTest.java
@@ -1,17 +1,12 @@
 package info.ejava.assignments.api.autorentals.svc.main.renter.client.mock;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.io.ByteArrayOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -22,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -30,19 +24,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-
 import info.ejava.assignments.api.autorenters.client.renters.RentersAPI;
 import info.ejava.assignments.api.autorenters.dto.renters.RenterDTO;
 import info.ejava.assignments.api.autorenters.dto.renters.RenterDTOFactory;
 import info.ejava.assignments.api.autorenters.dto.renters.RenterListDTO;
 import info.ejava.assignments.api.autorenters.svc.renters.RenterService;
 import info.ejava.assignments.api.autorenters.svc.renters.RentersController;
-import info.ejava.assignments.api.autorenters.svc.verify.MyController;
 import info.ejava.examples.common.dto.DtoUtil;
 import info.ejava.examples.common.dto.JsonUtil;
+import info.ejava.examples.common.exceptions.ClientErrorException;
 import lombok.extern.slf4j.Slf4j;
 
 //@WebMvcTest(RentersController.class)  // Test slice for mock web mvc
@@ -91,7 +81,7 @@ class RenterControllerMockTest {
 
     @Test
     //@Disabled
-    void should_find_all_test() throws Exception {
+    void should_find_all_renters() throws Exception {
         // String jsonResponse = """
         //         [ {
         //                 "id" : "2",
@@ -128,4 +118,27 @@ class RenterControllerMockTest {
         verify(renterServiceMock,times(1)).getRenters(any(Pageable.class));
         BDDMockito.then(renterServiceMock).should(times(1)).getRenters(any(Pageable.class));
     }
+
+    @Test
+    void should_find_given_renter_by_valid_id() throws Exception{
+
+        String jsonResponse = dtoUtil.marshal(renters.getRenters().get(0));
+        BDDMockito.when(renterServiceMock.getRenter(anyString())).thenReturn(renters.getRenters().get(0));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(RentersAPI.RENTER_PATH.replace("{id}", renters.getRenters().get(0).getId())))
+                            .andExpect(MockMvcResultMatchers.status().isOk())
+                            .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+    }
+
+    @Test
+    void should_not_find_given_renter_by_invalid_id() throws Exception{
+
+        BDDMockito.when(renterServiceMock.getRenter(anyString()))
+                    .thenThrow(ClientErrorException.NotFoundException.class);
+        String uri = RentersAPI.RENTER_PATH.replace("{id}", renters.getRenters().get(0).getId());
+        log.info("path - {}",uri);
+        mockMvc.perform(MockMvcRequestBuilders.get(uri))
+                            .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
 }
