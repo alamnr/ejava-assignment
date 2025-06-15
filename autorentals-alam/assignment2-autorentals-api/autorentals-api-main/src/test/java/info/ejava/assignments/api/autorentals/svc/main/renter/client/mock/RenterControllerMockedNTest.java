d04c8b9b9966caf5c0312ed92c0322c780c1557c
alamnr <alamnr@gmail.com>
2025-06-15 12:51:58 +0600

    added mock unit test for Rentr controller

--- autorentals-alam/assignment2-autorentals-api/autorentals-api-main/src/test/java/info/ejava/assignments/api/autorentals/svc/main/renter/client/mock/RenterControllerMockedNTest.java
+++ autorentals-alam/assignment2-autorentals-api/autorentals-api-main/src/test/java/info/ejava/assignments/api/autorentals/svc/main/renter/client/mock/RenterControllerMockedNTest.java
@@ -30,6 +30,7 @@ import info.ejava.assignments.api.autorenters.dto.renters.RenterDTOFactory;
 import info.ejava.assignments.api.autorenters.dto.renters.RenterListDTO;
 import info.ejava.assignments.api.autorenters.svc.renters.RenterService;
 import info.ejava.assignments.api.autorenters.svc.renters.RentersController;
+import info.ejava.assignments.api.autorenters.svc.renters.RentersExceptionAdvice;
 import info.ejava.examples.common.dto.DtoUtil;
 import info.ejava.examples.common.dto.JsonUtil;
 import info.ejava.examples.common.exceptions.ClientErrorException;
@@ -37,7 +38,7 @@ import lombok.extern.slf4j.Slf4j;
 
 //@WebMvcTest(RentersController.class)  // Test slice for mock web mvc
 @WebMvcTest
-@Import(RentersController.class)
+@Import(value = {RentersController.class,RentersExceptionAdvice.class})
 //@Import(MyController.class)
 //@WebMvcTest(MyController.class)
 @AutoConfigureMockMvc
@@ -139,6 +140,95 @@ class RenterControllerMockTest {
         log.info("path - {}",uri);
         mockMvc.perform(MockMvcRequestBuilders.get(uri))
                             .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void should_create_new_post_when_renter_is_valid() throws Exception{
+        RenterDTO withID = renters.getRenters().get(0);
+        RenterDTO withNullID = withID.withId(null);
+
+        String json = dtoUtil.marshal(withNullID);
+        //BDDMockito.when(renterServiceMock.createRenter(any(RenterDTO.class))).thenReturn(renters.getRenters().get(0));
+        BDDMockito.when(renterServiceMock.createRenter(withNullID)).thenReturn(withID);
+        mockMvc.perform(MockMvcRequestBuilders.post(RentersAPI.RENTERS_PATH)
+                        .contentType("application/json")
+                        .content(json))
+                        .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+     @Test
+    void should_create_new_post_when_renter_is_invalid() throws Exception{
+        RenterDTO withID = renters.getRenters().get(0);
+        RenterDTO withNullInvalidID = withID.withId(null);
+        withNullInvalidID.withFirstName("");
+        withNullInvalidID.withEmail("");
+
+        String json = dtoUtil.marshal(withNullInvalidID);
+        
+        BDDMockito.when(renterServiceMock.createRenter(withNullInvalidID)).thenThrow(ClientErrorException.InvalidInputException.class);
+        mockMvc.perform(MockMvcRequestBuilders.post(RentersAPI.RENTERS_PATH)
+                        .contentType("application/json")
+                        .content(json))
+                        .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+    }
+
+
+    @Test
+    void should_update_post_when_given_valid_post() throws Exception {
+        RenterDTO renterWithoutId = renterDTOFactory.listBuilder().make(1, 1).getRenters().get(0);
+        renterWithoutId = renterWithoutId.withFirstName("updated - "+renterWithoutId.getFirstName());
+        String id = "renter-123";
+        RenterDTO renterWithId = renterWithoutId.withId(id);
+        String json = dtoUtil.marshal(renterWithoutId);
+        BDDMockito.when(renterServiceMock.updateRenter(id, renterWithoutId)).thenReturn(renterWithId);
+        String uri = RentersAPI.RENTER_PATH.replace("{id}", id);
+        mockMvc.perform(MockMvcRequestBuilders.put(uri)
+                                    .contentType("application/json")
+                                    .content(json))
+                                    .andExpect(MockMvcResultMatchers.status().isOk());
+
+
+
+    }
+
+    @Test
+    void should_delete_renter_with_given_id() throws Exception {
+
+        String uri = RentersAPI.RENTER_PATH.replace("{id}", "renter-1");
+        mockMvc.perform(MockMvcRequestBuilders.delete(uri))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+        
+        verify(renterServiceMock,times(1)).removeRenter(anyString());
+        BDDMockito.then(renterServiceMock).should(times(1)).removeRenter(anyString());
+    }
+
+    
+    @Test
+    void should_delete_all_renters() throws Exception {
+
+        
+        mockMvc.perform(MockMvcRequestBuilders.delete(RentersAPI.RENTERS_PATH))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+        
+        verify(renterServiceMock,times(1)).removeAllRenters();
+        BDDMockito.then(renterServiceMock).should(times(1)).removeAllRenters();
+    }
+
+    @Test
+    void should_check_has_renter() throws Exception {
+        BDDMockito.when(renterServiceMock.hasRenter(anyString())).thenReturn(true);
+        String uri = RentersAPI.RENTER_PATH.replace("{id}", "renter-123");
+        mockMvc.perform(MockMvcRequestBuilders.head(uri))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        BDDMockito.when(renterServiceMock.hasRenter(anyString())).thenReturn(false);
+        String uri2 = RentersAPI.RENTER_PATH.replace("{id}", "renter-321");
+        mockMvc.perform(MockMvcRequestBuilders.head(uri2))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        verify(renterServiceMock,times(2)).hasRenter(anyString());
+        BDDMockito.then(renterServiceMock).should(times(2)).hasRenter(anyString());
+    
     }
 
 }
